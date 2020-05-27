9517c1a20d1392abae3b9db8e747cb143cf9516a
tit-huberthanczewski <64531746+tit-huberthanczewski@users.noreply.github.com>
2020-05-27 09:07:51 +0200

    Add files via upload

---
+++ java/skrypt.js
@@ -0,0 +1,41 @@
+function zmien1()
+{
+	document.getElementById('foto').src="foto1.png";
+}
+function zmien2()
+{
+	document.getElementById('foto').src="foto2.png";
+}
+
+
+
+function zmien3()
+{
+	document.getElementById('foto2').src="foto3.png";
+}
+function zmien4()
+{
+	document.getElementById('foto2').src="foto4.png";
+}
+
+
+
+function zmien5()
+{
+	document.getElementById('foto3').src="foto5.png";
+}
+function zmien6()
+{
+	document.getElementById('foto3').src="foto6.png";
+}
+
+
+
+function zmien7()
+{
+	document.getElementById('foto4').src="foto7.png";
+}
+function zmien8()
+{
+	document.getElementById('foto4').src="foto8.png";
+}
