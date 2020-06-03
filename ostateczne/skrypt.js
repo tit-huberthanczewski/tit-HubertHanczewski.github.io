7ccd7b331941f8e761c0e89cf06cccc2dadc5ac0
tit-huberthanczewski <64531746+tit-huberthanczewski@users.noreply.github.com>
2020-06-03 10:02:02 +0200

    Add files via upload

---
+++ ostateczne/skrypt.js
@@ -0,0 +1,19 @@
+function zmien1()
+{
+	document.getElementById('foto').src="foto1.png";
+}
+function zmien2()
+{
+	document.getElementById('foto').src="foto2.png";
+}
+
+
+
+function zmien3()
+{
+	document.getElementById('foto2').src="foto3.png";
+}
+function zmien4()
+{
+	document.getElementById('foto2').src="foto4.png";
+}
